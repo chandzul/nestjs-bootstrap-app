40f79f71f3a94f6060ab89b3ab95eedea0542bee
David Chandzul <dchandzul@gmail.com>
2022-12-26 22:18:01 -0600

    add logic to use jwt access token

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,7 +1,15 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SignUpDto } from './dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto';
+import { Response } from 'express';
 
 @Controller('authentication')
 export class AuthenticationController {
@@ -11,6 +19,20 @@ export class AuthenticationController {
   signUp(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(signUpDto);
   }
+
+  // @HttpCode(HttpStatus.OK)
+  // @Post('sign-in')
+  // async signIn(
+  //   @Res({ passthrough: true }) response: Response,
+  //   @Body() signInDto: SignInDto,
+  // ) {
+  //   const accessToken = await this.authService.signIn(signInDto);
+  //   response.cookie('accessToken', accessToken, {
+  //     secure: true,
+  //     httpOnly: true,
+  //     sameSite: true,
+  //   });
+  // }
 
   @HttpCode(HttpStatus.OK)
   @Post('sign-in')
--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -10,12 +10,18 @@ import { Repository } from 'typeorm';
 import { HashingService } from '../hashing/hashing.service';
 import { SignUpDto } from './dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto';
+import { JwtService } from '@nestjs/jwt';
+import jwtConfig from '../config/jwt.config';
+import { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
     private readonly hashingService: HashingService,
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
   ) {}
 
   async signUp(signUpDto: SignUpDto) {
@@ -55,6 +61,19 @@ export class AuthenticationService {
       throw new UnauthorizedException('Password does not match');
     }
 
-    return true;
+    const accessToken = await this.jwtService.signAsync(
+      {
+        sub: user.id,
+        email: user.email,
+      },
+      {
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+        expiresIn: this.jwtConfiguration.accessTokenTll,
+      },
+    );
+
+    return { accessToken };
   }
 }
