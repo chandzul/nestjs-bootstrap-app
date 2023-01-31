0dcba1325fe72d20ca7ce0686f03ad9fd9419393
David Chandzul <dchandzul@gmail.com>
2023-01-30 23:11:35 -0600

    Claims based authorization

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -13,6 +13,7 @@ import { AccessTokenGuard } from './authentication/guards/access-token.guard';
 import { AuthenticationGuard } from './authentication/guards/authentication.guard';
 import { RefreshTokenIdsStorage } from './authentication/refresh-token-ids.storage';
 import { RolesGuard } from './authorization/guards/roles.guard';
+import { PermissionsGuard } from './authorization/guards/permissions.guard';
 
 @Module({
   imports: [
@@ -36,7 +37,7 @@ import { RolesGuard } from './authorization/guards/roles.guard';
     },
     {
       provide: APP_GUARD,
-      useClass: RolesGuard,
+      useClass: PermissionsGuard, // RolesGuard,
     },
     AccessTokenGuard,
     RefreshTokenIdsStorage,
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,10 @@
 import { BaseEntity } from 'src/config/base.entity';
 import { Entity, Column } from 'typeorm';
 import { Role } from '../enums/role.enum';
+import {
+  Permission,
+  PermissionType,
+} from '../../iam/authorization/permission.type';
 
 @Entity({ name: 'users' })
 export class User extends BaseEntity {
@@ -12,4 +16,10 @@ export class User extends BaseEntity {
 
   @Column({ enum: Role, default: Role.Regular })
   role: Role;
+
+  // NOTE: Having the "permissions" column in combination with the "role"
+  // likely does not make sense. We use both in this course just to showcase
+  // two different approaches to authorization.
+  @Column({ enum: Permission, default: [], type: 'json' })
+  permissions: PermissionType[];
 }
